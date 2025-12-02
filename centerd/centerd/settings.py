f3ea95d16f5620e6e73c5cc9af4f5be378583149
nilrey inter arm <nilrey@gmail.com>
2025-12-02 23:12:18 +0300

    switch db to postgres

--- centerd/centerd/settings.py
+++ centerd/centerd/settings.py
@@ -77,11 +77,22 @@ WSGI_APPLICATION = "centerd.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'eif_db',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
 
 # Password validation
---
+++ centerd/inform_objects/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class InformObjectsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'inform_objects'
