e229a986393a66aa8c44138f7866f1207b3b8c29
nilrey inter arm <nilrey@gmail.com>
2026-01-28 14:59:26 +0300

    Убраны лишние блоки

--- centerd/pages/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Review
-
-@admin.register(Review)
-class ReviewAdmin(admin.ModelAdmin):
-    list_display = ['text', 'created_date', 'is_published']
-    list_filter = ['created_date', 'is_published']
-    search_fields = ['text']
